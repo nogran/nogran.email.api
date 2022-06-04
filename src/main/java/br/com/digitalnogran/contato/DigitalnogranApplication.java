f050fb54e200c41f89cfe216ee679c819cb7828c
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-04 13:36:57 -0300

    create project

---
+++ src/main/java/br/com/digitalnogran/contato/DigitalnogranApplication.java
@@ -0,0 +1,13 @@
+package br.com.digitalnogran.contato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalnogranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalnogranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digitalnogran/contato/DigitalnogranApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalnogran.contato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalnogranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
