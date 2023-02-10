b117f5957c00f005d7fa319ae323af3af338c9bd
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2023-02-10 00:16:31 -0300

    fix/controllerTest

--- src/test/java/com/nogran/email/api/controller/ContactControllerTest.java
+++ src/test/java/com/nogran/email/api/controller/ContactControllerTest.java
@@ -7,7 +7,6 @@ import com.nogran.email.api.service.EmailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,28 +19,27 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@WebMvcTest
-@SpringBootTest
+@WebMvcTest
 public class ContactControllerTest {
-//    @Autowired
-//    private MockMvc mockMvc;
-//    @MockBean
-//    private ContactService contactService;
-//    @MockBean
-//    private EmailService emailService;
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private ContactService contactService;
+    @MockBean
+    private EmailService emailService;
 
-//    @Test
-//    public void findAll_ShouldReturnContacts() throws Exception {
-//        List<ContactDTO> contacts = ContactDTOMock.createContacts();
-//
-//        when(contactService.getAll()).thenReturn(contacts);
-//
-//        mockMvc.perform(get("/api/v1/")
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$[0].name", is("John Doe")))
-//                .andExpect(jsonPath("$[0].email", is("john.doe@email.com")))
-//                .andExpect(jsonPath("$[1].name", is("Jane Doe")))
-//                .andExpect(jsonPath("$[1].email", is("jane.doe@email.com")));
-//    }
+    @Test
+    public void findAll_ShouldReturnContacts() throws Exception {
+        List<ContactDTO> contacts = ContactDTOMock.createContacts();
+
+        when(contactService.getAll()).thenReturn(contacts);
+
+        mockMvc.perform(get("/api/v1/")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name", is("John Doe")))
+                .andExpect(jsonPath("$[0].email", is("john.doe@email.com")))
+                .andExpect(jsonPath("$[1].name", is("Jane Doe")))
+                .andExpect(jsonPath("$[1].email", is("jane.doe@email.com")));
+    }
 }
