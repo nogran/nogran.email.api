a79283b8c5e15691eb95cf10c6e29b44d08c47b2
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2023-02-11 18:08:26 -0300

    update class to test

--- nogran.email.api/src/test/java/com/nogran/email/api/NogranEmailApplicationTests.java
+++ nogran.email.api/src/test/java/com/nogran/email/api/NogranEmailApplicationTests.java
@@ -3,7 +3,7 @@ package com.nogran.email.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = NogranEmailApplicationTests.class)
 class NogranEmailApplicationTests {
 
 	@Test
